420c11bb4995dfe570c672df23bdc300a8cee271
Michael Zeevi <michael.52@msn.com>
2020-10-23 14:04:29 +0300

    added outputs for vpc

---
+++ outputs.tf
@@ -0,0 +1,24 @@
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = module.vpc.vpc_id
+}
+
+output "vpc_cidr_block" {
+  description = "The CIDR block of the VPC"
+  value       = module.vpc.vpc_cidr_block
+}
+
+output "availability_zones" {
+  description = "The availability zones used in the VPC."
+  value       = module.vpc.azs
+}
+
+output "public_subnets_cidr_blocks" {
+  description = "List of cidr_blocks of public subnets"
+  value       = module.vpc.public_subnets_cidr_blocks
+}
+
+output "private_subnets_cidr_blocks" {
+  description = "List of cidr_blocks of private subnets"
+  value       = module.vpc.private_subnets_cidr_blocks
+}
