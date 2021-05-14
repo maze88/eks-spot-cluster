57d4e4735c441b585ceb35b49c11cba17f53dca9
Michael Zeevi <michael.52@msn.com>
2021-05-14 12:14:20 +0300

    split main.tf to eks and vpc

--- eks.tf
+++ eks.tf
@@ -1,24 +1,3 @@
-data "aws_availability_zones" "available" {
-  state = "available"
-}
-
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name                 = var.project_name
-  cidr                 = var.vpc_cidr
-  azs                  = data.aws_availability_zones.available.names
-  public_subnets       = var.public_subnets
-  private_subnets      = var.private_subnets
-  enable_dns_hostnames = true
-  #enable_dns_support   = true
-
-  public_subnet_tags  = { "kubernetes.io/cluster/${var.project_name}" = "shared" }
-  tags = {
-    "Env" = var.env_name
-  }
-}
-
 module "eks" {
   source = "terraform-aws-modules/eks/aws"
 
